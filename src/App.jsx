8ca23a2f04112ecf0409877b5e5116b808e3e509
[CLOUD]나용진 <yjnaga@naver.com>
2024-10-02 15:25:40 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/Home";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/Signup.jsx
@@ -0,0 +1,11 @@
+import Button from "../components/Button";
+
+const Signup = () => {
+  return (
+    <>
+      <Button text={"회원가입"} />
+    </>
+  );
+};
+
+export default Signup;
