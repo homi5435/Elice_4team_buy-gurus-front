d79053331bb0d4d0e5536cef0285e2350b4d1d1d
[CLOUD]나용진 <yjnaga@naver.com>
2024-10-08 11:18:21 +0900

    resolve conflicts

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,26 @@ import Home from "./pages/Home";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
 import MyPage from "./pages/MyPage";
+import Order from "./pages/Order/Order";
+import OrderDetail from "./pages/Order/OrderDetail";
+import AccordionTest from "./pages/AccordionTest";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/home" element={<Home />} />
+        <Route path="/" element={<Home />} exact/>
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/userMe" element={<MyPage />} />
+        <Route path="/order" element={<Outlet />} >
+          <Route index element={<Order/>}/>
+          <Route path=":orderId" element={<OrderDetail />} />
+        </Route>
+        <Route path="/test" element={<AccordionTest />}/>
       </Routes>
     </>
   );
-}
+
 
 export default App;
