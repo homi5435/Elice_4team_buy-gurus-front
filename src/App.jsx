1cb4abc8511a0f7c9a7e4e68bbe70733788f4a9a
[CLOUD]나용진 <yjnaga@naver.com>
2024-10-08 11:04:26 +0900

    add .env file

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,16 @@ import "./App.css";
 import Home from "./pages/Home";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
+import MyPage from "./pages/MyPage";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/userMe" element={<MyPage />} />
       </Routes>
     </>
   );
