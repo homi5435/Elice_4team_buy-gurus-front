00d040fd1164e0c49704cae5aa35c65aa901d982
PromotezCitizen <gkstkdgus821@gmail.com>
2024-10-08 10:27:10 +0900

    Fix: Modal 동작 방식 수정
    useEffect 제거 및 stopPropagation을 이용한 Bubbling 방지로 원하던 동작 구현

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,24 +1,12 @@
-import React, {useEffect} from "react";
+import React from "react";
 import './Modal.css'; // 스타일을 추가할 CSS 파일
 
 const Modal = ({ isOpen, onClose, children }) => {
-  useEffect(() => {
-    const handleClickOutside = (e) => {
-      const modal = document.querySelector(".modal-content");
-
-      // 이 부분을 어떻게든 해결해야하는데...
-      // if (e.target === document.querySelector("#root > div > div:nth-child(5) > button")) return;
-      if (!modal.contains(e.target)) onClose();
-    }
-    isOpen ? document.addEventListener('click', handleClickOutside) : document.removeEventListener('click', handleClickOutside);
-    return () => document.removeEventListener('click', handleClickOutside);
-  }, [isOpen])
-
   if (!isOpen) return null;
 
   return (
-    <div id="modal" className="modal-overlay">
-      <div className="modal-content">
+    <div className="modal-overlay" onClick={onClose}>
+      <div className="modal-content" onClick={(e) => e.stopPropagation()}>
         <button className="close-button" onClick={onClose}>
           &times;
         </button>
