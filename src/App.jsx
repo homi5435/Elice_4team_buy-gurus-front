6cba86c68da58487f9ea9156552f29d7f185bbb2
[CLOUD]나용진 <yjnaga@naver.com>
2024-10-08 11:25:21 +0900

    resolve conflicts(2)

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,10 @@ import "./App.css";
 import Home from "./pages/Home";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
-import MyPage from "./pages/MyPage";
 import Order from "./pages/Order/Order";
 import OrderDetail from "./pages/Order/OrderDetail";
 import AccordionTest from "./pages/AccordionTest";
+import MyPage from "./pages/MyPage";
 
 function App() {
   return (
@@ -15,12 +15,11 @@ function App() {
         <Route path="/" element={<Home />} exact/>
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/userMe" element={<MyPage />} />
         <Route path="/order" element={<Outlet />} >
-          <Route index element={<Order/>}/>
-          <Route path=":orderId" element={<OrderDetail />} />
-        </Route>
+        <Route index element={<Order/>}/>
+        <Route path=":orderId" element={<OrderDetail />} />
         <Route path="/test" element={<AccordionTest />}/>
+        <Route path="/userMe" element={<MyPage />} />
       </Routes>
     </>
   );
