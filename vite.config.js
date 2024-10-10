4c7febc74d3c68b778d4b08235de4f49f77106a0
PromotezCitizen <gkstkdgus821@gmail.com>
2024-10-10 17:09:27 +0900

    config: import 시 경로 설정
    `/src/pages/~` 에서 `@/pages/~` 로 import 가능하도록 설정
    기존의 `/src/pages/~`로도 import 가능

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,11 @@ import * as path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias:{
+      '@': path.resolve(__dirname, 'src'),
+    }
+  },
   server: {
     proxy: {
       '/api': 'http://localhost:8080'
