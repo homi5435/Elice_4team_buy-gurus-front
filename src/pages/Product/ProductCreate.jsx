8a84c472a20fe460ac8f906656218038ba862552
[CLOUD4] 홍민영 <47109@naver.com>
2024-10-21 17:10:13 +0900

    Feat: 카테고리 조회 API 호출 및 상품 추가 API 호출

--- src/pages/Product/ProductCreate.jsx
+++ src/pages/Product/ProductCreate.jsx
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Form, Button, Col, Row, Container, Image } from 'react-bootstrap';
 import Header from '/src/components/Header';
+import axios from 'axios';
 
 const ProductCreate = () => {
   // 폼 데이터
@@ -13,9 +14,43 @@ const ProductCreate = () => {
     imageFiles: []
   });
 
+  // 카테고리 데이터
+  const [categories, setCategories] = useState([]);
+  const [subCategories, setSubCategories] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState('');
+
   // 파일 미리보기, 파일 개수
   const [previewImages, setPreviewImages] = useState([]);
   const [fileCount, setFileCount] = useState(0);
+
+  // 카테고리 조회
+  useEffect(() => {
+    axios.get('/api/category')
+        .then(response => setCategories(response.data))
+        .catch(error => console.log(error));
+    }, []);
+
+  // 대분류 선택 시 소분류 업데이트
+  const handleCategoryChange = (e) => {
+    const categoryId = e.target.value; // 대분류에서 선택한 CategoryId
+    setSelectedCategory(categoryId);
+    const selected = categories.find((category) => category.id === parseInt(categoryId));
+    setSubCategories(selected ? selected.children : []); // 소분류 또는 []
+
+    // 초기화
+    setFormData((prevState) => ({
+        ...prevState,
+        category: '',
+      }));
+  };
+
+    // 소분류 선택 시 소분류 ID를 formData에 저장
+    const handleSubCategoryChange = (e) => {
+        setFormData((prevState) => ({
+          ...prevState,
+          category: e.target.value,
+        }));
+      };
 
   const handleChange = (e) => {
     setFormData({
@@ -41,6 +76,18 @@ const ProductCreate = () => {
   // 상품 추가
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    const data = new FormData();
+    data.append('name', formData.name);
+    data.append('price', formData.price);
+    data.append('description', formData.description);
+    data.append('quantity', formData.quantity);
+    data.append('category', formData.category);
+    formData.imageFiles.forEach((file) => {
+      data.append('imageFiles', file);
+    });
+
+    axios.post('/api/product', data)
   };
 
   return (
@@ -102,15 +149,37 @@ const ProductCreate = () => {
             </Form.Group>
 
             <Form.Group controlId="formCategory">
-              <Form.Label>카테고리</Form.Label>
+              <Form.Label>상품 대분류</Form.Label>
               <Form.Control
-                type="text"
+                as="select"
+                onChange={handleCategoryChange}
+                required
+              >
+                <option value="">대분류를 선택하세요.</option>
+                {categories.map((category) => (
+                  <option key={category.id} value={category.id}>
+                    {category.name}
+                  </option>
+                ))}
+              </Form.Control>
+            </Form.Group>
+
+            <Form.Group controlId="formSubCategory">
+              <Form.Label>상품 소분류</Form.Label>
+              <Form.Control
+                as="select"
                 name="category"
                 value={formData.category}
-                onChange={handleChange}
-                placeholder="카테고리를 입력하세요."
+                onChange={handleSubCategoryChange}
                 required
-              />
+              >
+                <option value="">소분류를 선택하세요.</option>
+                {subCategories.map((subCategory) => (
+                  <option key={subCategory.id} value={subCategory.id}>
+                    {subCategory.name}
+                  </option>
+                ))}
+              </Form.Control>
             </Form.Group>
 
             <Form.Group controlId="formFile">
